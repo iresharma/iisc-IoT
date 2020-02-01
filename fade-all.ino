d6771f86770d843d6f6d99abc27797a71af23ff4
Iresh Sharma <32684272+iresharma@users.noreply.github.com>
2020-02-01 16:14:54 +0530

    Update and rename fade all.ino to fade-all.ino

--- fade-all.ino
+++ fade-all.ino
@@ -10,7 +10,14 @@ int led8 = D8;
 int brightness = 0;   
 int fadeAmount = 10;    
 void setup() {
-  pinMode(led, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT);
+  pinMode(led8, OUTPUT);
 }
 
 void loop() {
