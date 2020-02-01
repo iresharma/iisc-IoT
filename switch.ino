b141519992b2e87ddfea9370900f9b2a1ea5bab4
Iresh Sharma <32684272+iresharma@users.noreply.github.com>
2020-02-01 16:39:05 +0530

    Create switch.ino

---
+++ switch.ino
@@ -0,0 +1,48 @@
+
+int led1 = D1;
+int led2 = D2;
+int led3 = D3;
+int led4 = D4;
+int led5 = D5;
+int led6 = D6;
+int led7 = D7;
+int led8 = D8;   
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT);
+  pinMode(led8, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  char data = Serial.read();
+  switch(data) {
+    case '1':
+      
+      digitalWrite(D1, LOW);
+      digitalWrite(D3, LOW);
+      digitalWrite(D5, LOW);
+      digitalWrite(D7, LOW);
+      digitalWrite(D2, HIGH);
+      digitalWrite(D4, HIGH);
+      digitalWrite(D6, HIGH);
+      digitalWrite(D8, HIGH);
+      break;
+    case '0': 
+      
+        digitalWrite(D1, LOW);
+        digitalWrite(D3, LOW);
+        digitalWrite(D5, LOW);
+        digitalWrite(D7, LOW);
+        digitalWrite(D2, HIGH);
+        digitalWrite(D4, HIGH);
+        digitalWrite(D6, HIGH);
+        digitalWrite(D8, HIGH);
+        break;
+  }
+}
