1819a63916e919eea865516db3f8ba5d5f498097
Iresh Sharma <32684272+iresharma@users.noreply.github.com>
2020-02-02 12:56:13 +0530

    Create DHT11.ino

---
+++ DHT11.ino
@@ -0,0 +1,47 @@
+#define BLYNK_PRINT Serial
+ 
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+ 
+char auth[] = "";
+ 
+char ssid[] = "iresharma";
+char pass[] = "iresharma88";
+ 
+#define DHTPIN D6          
+ 
+#define DHTTYPE DHT11     
+ 
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+ 
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  Blynk.virtualWrite(V5, t);
+  Blynk.virtualWrite(V6, h);
+}
+ 
+void setup()
+{
+  Serial.begin(9600);
+ 
+  Blynk.begin(auth, ssid, pass);
+  dht.begin();
+ 
+  timer.setInterval(1000L, sendSensor);
+}
+
+
+ 
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
